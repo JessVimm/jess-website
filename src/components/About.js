2835aa1ba3f45b9a984c9c14ccbeecacc8c3aed5
Jess Vimm <jessvimm@gmail.com>
2022-12-21 17:12:19 -0600

    About.js: Updated About section

--- src/components/About.js
+++ src/components/About.js
@@ -33,10 +33,14 @@ export default function About() {
                         In July of 2021, I graduated from college. I started working for a local company for
                         a couple of months where I created a technical manual for a software project. In 2022, I 
                         entered Infosys as a Software Engineer to learn about backend development with Java and Angular.
+                    </h2>
+                    <h2 className="text-lg text-purple-700 flex justify-center mb-12 canada">
                         Later, I created my own web page (this one) to share how I work with my projects, ideas and what technologies
                         I am currently learning and implementing. My interests go towards building applications from scratch, 
                         so any tech stack is welcome for me. I find excited the entire process of an application's lifecycle, 
                         from its birth to its production and maintainment.
+                    </h2>
+                    <h2 className="text-lg text-purple-700 flex justify-center mb-12 canada">
                         In my free time I am learning Chinese, cooking, reading, dancing or hanging out with my loved ones.
                         I also love bread and ice cream 🍞🍦
                     </h2>
